c6f0873bb2a69f0ddeec93086dec5bf2cb70732f
ti120 <itimur189@gmail.com>
2025-08-08 13:30:46 +0100

    Init

---
+++ app/src/main/java/com/example/vprdemo1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.vprdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.vprdemo1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'vprdemo1' library on application startup.
+    static {
+        System.loadLibrary("vprdemo1");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'vprdemo1' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
